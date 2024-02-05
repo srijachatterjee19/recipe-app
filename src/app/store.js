61498a35d8212377aaa26e524b2e1e50da68d4f5
srijachatterjee <sc1698@student.aru.ac.uk>
2024-02-05 11:21:08 +0000

    Refactored store.js and added each slice to seperate files

--- src/app/store.js
+++ src/app/store.js
@@ -1,96 +1,12 @@
-import allRecipesData from './data.js';
-import { configureStore,combineReducers } from 'redux';
-
-
-/*
-
-    Action Creators
-
-*/
-
-// Dispatched when the user types in the search input.
-// Sends the search term to the store.
-export const setSearchTerm = (term) => {
-  return { 
-    type: 'searchTerm/setSearchTerm', 
-    payload: term 
-  };
-}
-
-// Dispatched when the user presses the clear search button.
-export const clearSearchTerm = () => {
-  return { 
-    type: 'searchTerm/clearSearchTerm' 
-  };
-}
-
-// Dispatched when the user first opens the application.
-// Sends the allRecipesData array to the store.
-export const loadData = () => {
-  return {type:'allRecipes/loadData', payload: allRecipesData};
-
-}
-
-// Dispatched when the user clicks on the heart icon of 
-// a recipe in the "All Recipes" section.
-// Sends the recipe object to the store.
-export const addRecipe = (recipe) => {
-  return {type: 'favoriteRecipes/addRecipe',payload: recipe};
-}
-
-// Dispatched when the user clicks on the broken heart 
-// icon of a recipe in the "Favorite Recipes" section.
-// Sends the recipe object to the store.
-export const removeRecipe = (recipe) => {
-  return {type: 'favoriteRecipes/removeRecipe',payload: recipe };
-}
-
-/*
-
-    Reducers seperated
-
-*/
-
-const initialAllRecipes = [];
-const allRecipesReducer = (allRecipes = initialAllRecipes, action) => {
-  switch(action.type) {
-    case 'allRecipes/loadData':
-      return action.payload
-    default:
-      return allRecipes;
-  }
-}
-
-const initialSearchTerm = '';
-const searchTermReducer = (searchTerm = initialSearchTerm, action) => {
-  switch(action.type) {
-    case 'searchTerm/setSearchTerm':
-      return action.payload;
-    case 'searchTerm/clearSearchTerm':
-      return '';
-    default: 
-      return searchTerm;
-  }
-}
-const initialFavoriteRecipes = [];
-const favoriteRecipesReducer = (favoriteRecipes = initialFavoriteRecipes, action) => {
-  switch(action.type) {
-    case 'favoriteRecipes/addRecipe':
-      return  [...favoriteRecipes,action.payload];
-    case 'favoriteRecipes/removeRecipe':
-      return favoriteRecipes.filter(element => element.id !== action.payload.id);
-    default: 
-      return favoriteRecipes;
-  }
-}
-
+import { configureStore, combineReducers } from 'redux';
+import { favoriteRecipesReducer } from '../features/favoriteRecipes/favoriteRecipesSlice.js';
+import { allRecipesReducer } from '../features/allRecipes/allRecipesSlice.js';
+import { searchTermReducer } from '../features/searchTerm/searchTermSlice.js';
 
 const reducers = {
     allRecipes: allRecipesReducer,
     favoriteRecipes: favoriteRecipesReducer,
     searchTerm: searchTermReducer
-  };
-  
-  const rootReducer = combineReducers(reducers);
-  export const store = configureStore(rootReducer);
-  
+}
+
+export const store = configureStore(combineReducers(reducers))
